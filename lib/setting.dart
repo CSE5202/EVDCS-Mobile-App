4f37f9ea309dcdff774c29ab239d68666b968af2
tihetna1221 <tihetna2@gmail.com>
2020-03-09 20:01:18 +0100

    updatedversion of driver mobile app ui

--- lib/setting.dart
+++ lib/setting.dart
@@ -2,9 +2,36 @@ import 'package:flutter/material.dart';
 import 'page2.dart';
 class Setting extends StatelessWidget{
    @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
-    appBar: AppBar(title:Text('Settings')),
+      backgroundColor: Colors.grey.shade200,
+      appBar: AppBar(
+        elevation: 0,
+        brightness: Brightness.light,
+        backgroundColor: Colors.purple.withOpacity(0.5),
+      title: Text('Setting', style: TextStyle(color: Colors.white70,fontSize: 20.0,fontWeight: FontWeight.bold,)
+      ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text("Notification Setting",
+            style: TextStyle(color: Colors.indigo,fontSize: 20.0,fontWeight: FontWeight.bold,),
+            ),
+            SwitchListTile(
+              dense: true,
+              activeColor: Colors.purple,
+              contentPadding: const EdgeInsets.all(0),
+              value: true,
+              title: Text('Recieved notification'),
+              onChanged: (val){},
+            )
+          ],
 
+      ),
+      ),
+      
+      
     );
-    }}
+  }}
